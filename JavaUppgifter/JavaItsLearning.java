4fb796843ae1731ab2172dc1ceb4efa402c34dd9
ellebelleboo <eklelindholm@gmail.com>
2024-08-27 12:18:32 +0300

    Add files via upload

---
+++ JavaUppgifter/JavaItsLearning.java
@@ -0,0 +1,25 @@
+
+public class JavaItsLearning {
+
+    public static void main(String args[]) {
+        
+        String firstName = "Elle ";
+        String lastName = "Lindholm";
+        
+        int age = 18;
+        
+        double Height = 168;
+        
+        
+        boolean  isAStudent = true;
+        boolean isNotAStudent = false;
+        
+        
+       System.out.println(firstName + lastName); 
+       System.out.println(age);   
+       System.out.println("Is Elle a student?");
+       System.out.println(isAStudent);
+        System.out.println(Height + "cm");
+        
+    }
+}
