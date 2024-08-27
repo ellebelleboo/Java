6da1360c2a5e5dfea89fe640d70494764138bff0
ellebelleboo <eklelindholm@gmail.com>
2024-08-27 12:20:28 +0300

    Add files via upload

---
+++ JavaUppgifter/JavaItsLearning2.java
@@ -0,0 +1,15 @@
+
+public class JavaItsLearning2 {
+
+    public static void main(String args[]) {
+        int Num = 10;
+        int Num2 = 5;
+        System.out.println(Num + Num2);
+        System.out.println(Num - Num2);
+        System.out.println(Num * Num2);
+        System.out.println(Num / Num2);
+        
+             
+        
+    }
+}
