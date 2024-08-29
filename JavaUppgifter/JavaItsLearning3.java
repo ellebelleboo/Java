90b60877795f0f64bea6e8d0001daca2bd9a662c
ellebelleboo <eklelindholm@gmail.com>
2024-08-29 12:09:31 +0300

    Add files via upload

---
+++ JavaUppgifter/JavaItsLearning3.java
@@ -0,0 +1,17 @@
+
+public class JavaItsLearning3 {
+
+   
+    public static void main(String args[]) {
+        int var1 = 12;
+        float var2 = 2.0f;
+        double var3 = 3;
+        char var4 = 'A';
+        
+        System.out.println("var1 printed as double: " +(double)var1);
+        System.out.println("var2 printed as double: " +(double)var2);
+        System.out.println("var3 printed as int: " +(int)var3);
+        System.out.println("var4 printed as int: " +(int)var4);
+        System.out.println("var1 printed as byte: " +(byte)var1);
+    }
+}
---
+++ JavaUppgifter/JavaItsLearning4.java
@@ -0,0 +1,13 @@
+public class JavaItsLearning4 {
+
+
+    public static void main(String args[]) {
+        for( int i = 5; i <= 20; i++){
+        if(i % 2 == 0){
+            continue;
+        }
+            System.out.println(i);
+    }
+     
+    }
+}
